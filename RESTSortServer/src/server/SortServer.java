01e6316becd00e8bbf2fb087a89f2d33caef6200
edi <sack4ra@googlemail.com>
2015-09-29 03:41:36 +0200

    disabled post method

--- RESTSortServer/src/server/SortServer.java
+++ RESTSortServer/src/server/SortServer.java
@@ -5,10 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -17,7 +15,6 @@ import javax.ws.rs.core.Response;
 
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 
 import helper.Helper;
 
@@ -69,47 +66,47 @@ public class SortServer {
 		return new JSONArray(list);
 	}
 
-	/**
-	 * 
-	 * POST service method, receives a JSON object of the form {"locale" :
-	 * String, "array" : String[]}, parses its parameters into a locale and an
-	 * JSON array, sorts the array with the corresponding locale collator and
-	 * returns it. If array or locale are not specified, default values from the
-	 * Helper class are used.
-	 * 
-	 * @param requestObject
-	 *            the JSONObject sent by the client
-	 * 
-	 * @return Response with status 200 (ok) and the sorted array in JSON format
-	 *         or Response with status 400 (invalid data) and no payload if the
-	 *         client data cannot be processed.
-	 */
-	@POST
-	@Path("/sort")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response sortJSONStringArray(JSONObject requestObject) {
-		try {
-			// create default parameters
-			JSONArray requestArray = new JSONArray(Helper.DEFAULT_REQUEST_ARRAY);
-			Locale locale = new Locale(Helper.DEFAULT_LOCALE);
-
-			// override default parameters with requestObject
-			if (requestObject.has(Helper.REQUEST_ARRAY_PARAMETER_NAME))
-				requestArray = requestObject.getJSONArray(Helper.REQUEST_ARRAY_PARAMETER_NAME);
-			if (requestObject.has(Helper.LOCALE_PARAMETER_NAME))
-				locale = new Locale(requestObject.getString(Helper.LOCALE_PARAMETER_NAME));
-
-			// sort and respond with ok()
-			JSONArray responsePayload = sort(requestArray, locale);
-			return Response.ok().entity(responsePayload).build();
-		} catch (JSONException e) {
-			// Send a response with status 400 (invalid data) in case the
-			// jsonArray cannot be processed properly.
-			return Response.status(400).build();
-		}
-
-	}
+//	/**
+//	 * 
+//	 * POST service method, receives a JSON object of the form {"locale" :
+//	 * String, "array" : String[]}, parses its parameters into a locale and an
+//	 * JSON array, sorts the array with the corresponding locale collator and
+//	 * returns it. If array or locale are not specified, default values from the
+//	 * Helper class are used.
+//	 * 
+//	 * @param requestObject
+//	 *            the JSONObject sent by the client
+//	 * 
+//	 * @return Response with status 200 (ok) and the sorted array in JSON format
+//	 *         or Response with status 400 (invalid data) and no payload if the
+//	 *         client data cannot be processed.
+//	 */
+//	@POST
+//	@Path("/sort")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response sortJSONStringArray(JSONObject requestObject) {
+//		try {
+//			// create default parameters
+//			JSONArray requestArray = new JSONArray(Helper.DEFAULT_REQUEST_ARRAY);
+//			Locale locale = new Locale(Helper.DEFAULT_LOCALE);
+//
+//			// override default parameters with requestObject
+//			if (requestObject.has(Helper.REQUEST_ARRAY_PARAMETER_NAME))
+//				requestArray = requestObject.getJSONArray(Helper.REQUEST_ARRAY_PARAMETER_NAME);
+//			if (requestObject.has(Helper.LOCALE_PARAMETER_NAME))
+//				locale = new Locale(requestObject.getString(Helper.LOCALE_PARAMETER_NAME));
+//
+//			// sort and respond with ok()
+//			JSONArray responsePayload = sort(requestArray, locale);
+//			return Response.ok().entity(responsePayload).build();
+//		} catch (JSONException e) {
+//			// Send a response with status 400 (invalid data) in case the
+//			// jsonArray cannot be processed properly.
+//			return Response.status(400).build();
+//		}
+//
+//	}
 
 	/**
 	 * GET service method, receives a JSON array and a locale string, sorts the
